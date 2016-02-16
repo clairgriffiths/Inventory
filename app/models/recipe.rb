65761d6435db07d324ba03bd93f9463ef719eb0c
cakeordeath83 <clair_griffiths@hotmail.com>
2016-02-16 14:05:08 +0000

    Doesn't save blank ingredients

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,4 +1,4 @@
 class Recipe < ActiveRecord::Base
-  has_many :ingredients
-  accepts_nested_attributes_for :ingredients
+  has_many :ingredients, :dependent => :destroy
+  accepts_nested_attributes_for :ingredients, :reject_if => lambda { |a| a[:name].blank? }, :allow_destroy => true
 end
